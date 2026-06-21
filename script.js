0e4e8a7621660f7a0e9622139ad9a9bb820ea6d4
Bhuvan <bhuvandhiman.pvt@gmail.com>
2026-06-21 14:30:23 +0530

    Added Error Handeling

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const button = document.getElementById("search-btn");
 button.addEventListener("click", getWeather);
 
 async function getWeather(){
+    const resultBox = document.getElementById("resultBox");
     const cityValue = document.getElementById("city-input").value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
     const response = await fetch(url);
+    if(!response.ok){
+        resultBox.innerHTML = `<div><p>City Not Found, Please try Again</p></div>`;
+        resultBox.classList.add("active");
+        return;
+    }
     const data = await response.json();
     const temperature = data.main.temp;
     const feelsLike = data.main.feels_like;
     const humidity = data.main.humidity;
     const description = data.weather[0].description;
 
-    const resultBox = document.getElementById("resultBox");
     resultBox.innerHTML =
     `<div>
-    <p>${cityValue} --- ${temperature}</p>
-    <p>Feels Like : ${feelsLike}</p>
+    <p>${cityValue} --- ${Math.round(temperature)} °C</p>
+    <p>Feels Like : ${Math.round(feelsLike)} °C</p>
     <p>Humidity : ${humidity}</p>
     <p>${description}</p>
     </div>`;
