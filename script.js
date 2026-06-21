e0ecf211d45ae1e57cc74495db9480d640c18d20
Bhuvan <bhuvandhiman.pvt@gmail.com>
2026-06-21 13:43:18 +0530

    display weather data in result box

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ async function getWeather(){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apiKey}&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    const temperature = data.main.temp;
+    const feelsLike = data.main.feels_like;
+    const humidity = data.main.humidity;
+    const description = data.weather[0].description;
+
+    const resultBox = document.getElementById("resultBox");
+    resultBox.innerHTML =
+    `<div>
+    <p>${cityValue} --- ${temperature}</p>
+    <p>Feels Like : ${feelsLike}</p>
+    <p>Humidity : ${humidity}</p>
+    <p>${description}</p>
+    </div>`;
+    resultBox.classList.add("active");
 }
